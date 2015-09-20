467487e6433bfdb90d16164bb135d86ed60afd4f
dlil <daniel.liljhammar@tretton37.com>
2015-09-20 14:32:38 +0200

    Invoke to ui thread in presenter.

--- LeetNESEmulator/MainForm.cs
+++ LeetNESEmulator/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace LeetNESEmulator
@@ -19,10 +20,12 @@ namespace LeetNESEmulator
 
         public void SetPixel(int pixelX, int pixelY, int paletteIndex)
         {
-            // Invoke check..coming from emulator thread.
-            // Slow as a dog, but fix later.
-            _surface.SetPixel(pixelX, pixelY, Color.Red);
-            Invalidate(new Rectangle(pixelX, pixelY, 1, 1));
+            Invoke(new Action(() =>
+            {
+                // Slow as a dog, but fix later.
+                _surface.SetPixel(pixelX, pixelY, Color.Red);
+                Invalidate(new Rectangle(pixelX, pixelY, 1, 1));    
+            }));
         }
 
         protected override void OnPaintBackground(PaintEventArgs e)
