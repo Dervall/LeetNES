9541e11814471c562f424e3ce7229c38e5519b60
dlil <daniel.liljhammar@tretton37.com>
2015-09-20 11:12:04 +0200

    Added very basic form as IRender implementation.

---
+++ LeetNESEmulator/IPresenter.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetNESEmulator
+{
+    public interface IPresenter
+    {
+        void SetPixel(int pixelX, int pixelY, int paletteIndex);
+    }
+}
---
+++ LeetNESEmulator/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace LeetNESEmulator
+{
+    public partial class MainForm : Form, IPresenter
+    {
+        private readonly Bitmap _surface = new Bitmap(256, 240);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetStyle(
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.UserPaint,
+                true);
+        }
+
+        public void SetPixel(int pixelX, int pixelY, int paletteIndex)
+        {
+            // Slow as a dog, but fix later.
+            _surface.SetPixel(pixelX, pixelY, Color.Red);
+            Invalidate(new Rectangle(pixelX, pixelY, 1, 1));
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawImageUnscaled(_surface, 0, 0);
+        }
+    }
+}
