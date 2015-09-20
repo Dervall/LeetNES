ec92419fccbb790ebef2b3e6a9f53ee36532edea
Per Dervall <pdervall@gmail.com>
2015-09-20 10:48:42 +0200

    Instructions master

--- LeetNES/ALU/Instructions/SEI.cs
+++ LeetNES/ALU/Instructions/SEI.cs
@@ -36,4 +36,41 @@ namespace LeetNES.ALU.Instructions
             cpuState.SetFlag(CpuState.Flags.InterruptDisable, true);
         }
     }
+
+    /*STX  Store Index X in Memory
+
+     X -> M                           N Z C I D V
+                                      - - - - - -
+
+     addressing    assembler    opc  bytes  cyles
+     --------------------------------------------
+     zeropage      STX oper      86    2     3
+     zeropage,Y    STX oper,Y    96    2     4
+     absolute      STX oper      8E    3     4*/
+    public class STX : BaseInstruction
+    {
+        public override string Mnemonic
+        {
+            get { return "STX"; }
+        }
+
+        public override IDictionary<byte, AddressingMode> Variants
+        {
+            get
+            {
+                return new Dictionary<byte, AddressingMode>
+                {
+                    {0x86, AddressingMode.ZeroPage},
+                    {0x96, AddressingMode.ZeroPageYIndexed},
+                    {0x8E, AddressingMode.Absolute},
+                };
+            }
+        }
+
+        protected override void InternalExecute(CpuState cpuState, IMemory memory, byte arg, Action<byte> write, ref int cycles)
+        {
+            write(cpuState.X);
+        }
+    }
+
 }
